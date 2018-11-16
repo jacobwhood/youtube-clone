c9e348ca352922fa52d63fb8166d164fe5c74d63
jacobwhood <jacobwhood@utexas.edu>
2018-11-16 16:30:10 -0600

    Correct webpack and babel config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+var SRC_DIR = path.join(__dirname, './client/src');
+var DIST_DIR = path.join(__dirname, './client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    }]
+  }
+};
