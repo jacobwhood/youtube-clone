3eefa14c8f7bbb254b1f263d0dfe6d3708345de3
jacobwhood <jacobwhood@utexas.edu>
2018-11-18 10:18:38 -0600

    Clean up server/index.js

--- server/index.js
+++ server/index.js
@@ -4,14 +4,13 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const request = require('request');
+const routes = require('./routes/routes.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
-app.use(cors());app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-const routes = require('./routes/routes.js');
 app.use('/api/', routes);
 
 
