c83eff11da6540935144e7033c1472aaade113c5
jacobwhood <jacobwhood@utexas.edu>
2018-11-21 17:29:48 -0600

    Initial config of React Router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect, browserHistory } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store/index.js';
 
+import LandingPage from './components/LandingPage.jsx';
+import Search from './components/Search.jsx';
+import UserSavedVideos from './components/UserSavedVideos.jsx';
+
 const App = () => (
   <div className='app-container'>
-    Hello, world
+    <Switch>
+      <Route exact path='/' render={props => <LandingPage  ...props/>} />
+      <Route exact path='/search' component={Search} />
+      <Route exact path='/users/:userId/videos' component={UserSavedVideos} />
+      <Redirect to='/' />
+    </Switch>
   </div>
 );
 
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>, document.getElementById('app')
+// );
+
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('app')
-);
+<BrowserRouter history={ browserHistory }>
+  <App />
+</BrowserRouter >, document.getElementById('app'));
