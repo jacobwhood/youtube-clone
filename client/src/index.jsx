1282db571361f38889505c2830ed6069d99673f1
jacobwhood <jacobwhood@utexas.edu>
2018-11-22 10:54:40 -0600

    Remove defining App component in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route, Redirect, browserHistory } from 'react-router-dom';
+import { BrowserRouter, browserHistory } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store/index.js';
 
-import LandingPage from './components/LandingPage.jsx';
-import Search from './components/Search.jsx';
-import UserSavedVideos from './components/UserSavedVideos.jsx';
-
-const App = () => (
-  <div className='app-container'>
-    <Switch>
-      <Route exact path='/' render={props => <LandingPage />} />
-      <Route exact path='/search' component={Search} />
-      <Route exact path='/users/:userId/videos' component={UserSavedVideos} />
-      <Redirect to='/' />
-    </Switch>
-  </div>
-);
+import App from './components/App.jsx';
 
 // ReactDOM.render(
 //   <Provider store={store}>
@@ -28,6 +15,6 @@ const App = () => (
 
 
 ReactDOM.render(
-<BrowserRouter history={ browserHistory }>
+<BrowserRouter history={browserHistory}>
   <App />
-</BrowserRouter >, document.getElementById('app'));
+</BrowserRouter>, document.getElementById('app'));
