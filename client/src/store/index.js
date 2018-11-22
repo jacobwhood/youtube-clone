530f5e1d06d6b2f0020cdb6f0dc94b12e5f3e62c
jacobwhood <jacobwhood@utexas.edu>
2018-11-22 10:55:01 -0600

    Define store for future use

---
+++ client/src/store/index.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { createStore, combineReducers } from 'redux';
+// import reducers from '../reducers/reducers.js';
+
+// export default store = createStore(
+//   combineReducers({
+//     state: reducers
+//   })
+// );
