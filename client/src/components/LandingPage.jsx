4bc94d2a3f40228628695b04aa2526ad0318fb92
jacobwhood <jacobwhood@utexas.edu>
2018-11-22 10:39:41 -0600

    Add props parameter to LandingPage

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-const LandingPage = () => (
+const LandingPage = (props) => (
   <div className='landing-page-container'>
     <h2>I am landing page!</h2>
   </div>
