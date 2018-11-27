519d475a84833852e7150ced1d22ebb647efd7cd
jacobwhood <jacobwhood@utexas.edu>
2018-11-27 17:36:23 -0600

    Define methods for database interaction layer

---
+++ server/util/database.js
@@ -0,0 +1,21 @@
+
+
+module.exports.saveVideoToDatabase = () => {
+
+};
+
+module.exports.updateVideoEntityInDatabase = () => {
+
+};
+
+module.exports.saveUserToDatabase = () => {
+
+};
+
+module.exports.retrieveUserFromDatabase = () => {
+
+};
+
+module.exports.removeUserFromDatabase = () => {
+
+};
