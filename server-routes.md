f24b1c5e97facdb2617bd7818c6fd9936ce53cd5
jacobwhood <jacobwhood@utexas.edu>
2018-11-27 21:13:39 -0600

    Rename public server routes
    Create mapping doc for public endpoints and rename existing endpoints

---
+++ server-routes.md
@@ -0,0 +1,5 @@
+// youtube routes
+//   videos-routes.js
+// GET   /videos/search/:query
+// POST  /videos/like  (userId, videoId)
+// GET   /videos/getlikedvideos
---
+++ server/routes/videos.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const youtubeUtil = require('../util/youtube.js');
+const db = require('../util/database.js');
+
+// GET /api/videos/search
+router.get('/search', (req, res) => {
+  let { query } = req.query;
+
+  youtubeUtil.searchVideos(query, (err, videos) => {
+    if (err) {
+      console.log('Error in Youtube util searchVideos cb: ', err);
+      res.sendStatus(500);
+    } else {
+      res.send(videos);
+    }
+  });
+});
+
+// POST  /videos/like  (userId, videoId)
+router.post('/like', (req, res) => {
+  let { userId, videoId } = req.query;
+
+
+});
+
+// GET   /videos/getlikedvideos
+router.get('/', (req, res) => {
+  let { userId } = req.query;
+
+  
+});
+
+module.exports = router;
