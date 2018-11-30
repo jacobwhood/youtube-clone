e7e2b5dff002ba56e682e3367001c54d24502f91
jacobwhood <jacobwhood@utexas.edu>
2018-11-29 21:11:36 -0600

    Render SearchBar, VideoList, and VideoPlayer

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import SearchBar from './SearchBar.jsx';
+import VideoList from './VideoList.jsx';
+import VideoPlayer from './VideoPlayer.jsx';
 
 const Search = () => (
   <div className='search-container'>
-    <h1>I am search!!</h1>
+    <h2>I am search!</h2>
+    <SearchBar />
+    <VideoList />
+    <VideoPlayer />
   </div>
 );
 
