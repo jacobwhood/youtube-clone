b0140a10f0954f1dd274dde10db9cf8dd5969fba
jacobwhood <jacobwhood@utexas.edu>
2018-11-29 21:12:39 -0600

    Add button to landing page to navigate to search

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 const LandingPage = (props) => (
   <div className='landing-page-container'>
     <h2>I am landing page!</h2>
+    <button><a href='/search'>Search</a></button>
   </div>
 );
 
